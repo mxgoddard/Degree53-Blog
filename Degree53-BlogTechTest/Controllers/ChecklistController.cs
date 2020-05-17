2b9f308707d49f54cb31f57d2d69657cd128df7e
mxgoddard <maxolivergoddard@gmail.com>
2020-05-17 23:56:59 +0100

    Add static checklist page

---
+++ Degree53-BlogTechTest/Controllers/ChecklistController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Degree53_BlogTechTest.Data.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Degree53_BlogTechTest.Controllers
+{
+    public class ChecklistController : Controller
+    {
+        private readonly ILogger<ChecklistController> _logger;
+        private readonly IBlogRepository _repo;
+
+        public ChecklistController(ILogger<ChecklistController> logger, IBlogRepository repo)
+        {
+            this._logger = logger;
+            this._repo = repo;
+        }
+
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
